fb70a5e5deca3bb77b52d1fcd71d647ed61c8cf8
divyansh <divyanshverma689@gmail.com>
2025-09-04 12:50:32 +0530

    practice

---
+++ tut9-project/main.py
@@ -0,0 +1,34 @@
+import random
+'''
+1 for snake
+-1 for water
+0 for gun
+
+'''
+
+computer = random.choice([-1, 0, 1])
+youstr = input("Enter you choice: ")
+youDict = {"s":1, "w":-1, "g":0}
+you = youDict[youstr]
+
+revDict = {1:"Snake", -1:"Water", 0:"Gun"}
+print(f"You choosen {revDict[you]} \nComputer Choosen {revDict[computer]}")
+
+
+if(computer == you):
+    print("Draw")
+else:
+    if(computer == -1 and you == 1):
+        print("You win")
+    elif(computer == -1 and you == 0):
+        print("You lose")
+    elif(computer == 1 and you == -1):
+        print("You lose")
+    elif(computer == 1 and you == 0):
+        print("You win")
+    elif(computer == 0 and you == -1):
+        print("You win")
+    elif(computer == 0 and you == 1):
+        print("You lose")
+    else:
+        print("Something went wrong")
